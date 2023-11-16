36b69bee652e2ffcf08d2f1620e4ee9b4fad89c9
martino1988 <61902639+martino1988@users.noreply.github.com>
2023-11-16 11:05:49 +0100

    Update document_parser.py

--- document_parser.py
+++ document_parser.py
@@ -1,8 +1,16 @@
-print("NOTE: document_parser is only tested with .txt and .csv files\n")
+import sys
 
-dok = input('Dateiname eingeben: ')
-param = input('Suchparameter eingeben: ')
-new_file = input('Zieldatei definieren: ')
+print("NOTE: document_parser is only tested with .txt and .csv files\nPlease note that the search is case sensitive")
+if len(sys.argv) != 4:
+    print("Invalid number of arguments")
+    print("Syntax: python.exe pdf_parser.py [document to parse] [term to search] [name of outputdocument]")
+    print("For example: python.exe pdf_parser.py presentation.pdf 'top secret' output.txt")
+    sys.exit()
+    
+
+dok = sys.argv[1]
+param = sys.argv[2]
+new_file = sys.argv[3]
 lines = []
 
 with open(dok, 'r', newline="") as file:
@@ -13,4 +21,3 @@ with open(dok, 'r', newline="") as file:
 with open (new_file, "w", newline="") as file:
     for line in lines:
         file.write(line)
-            
